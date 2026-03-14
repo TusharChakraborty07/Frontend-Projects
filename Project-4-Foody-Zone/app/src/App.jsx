1e77d2a1519011ce22abbd3cf2f3f5e56324fd9b
TusharChakraborty07 <chakrabortytushar684@gmail.com>
2026-03-15 01:01:03 +0530

    .env added

--- Project-4-Foody-Zone/app/src/App.jsx
+++ Project-4-Foody-Zone/app/src/App.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import nav_logo from "./assets/Foody Zone.svg";
 import SearchResult from "./components/SearchResult";
 
-export const BASE_URL = "http://localhost:9000";
+export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
 const App = () => {
   const [data, setData] = useState([]);
